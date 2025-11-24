026b465b5bee9e15c608ec1abc03d52401f824ed
Ivan Herald <ivanheraldd@gmail.com>
2025-11-24 19:10:46 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 
-;
+
 
 dotenv.config();
 
@@ -40,8 +40,8 @@ app.use("/lgc",CastingRouter)
 mongoose.connect(process.env.MONGO_URI) 
 
   .then(() => {
-    app.listen(process.env.PORT || 5000, () => {
-      console.log("Connected to Database And Server is running");
+    app.listen(process.env.PORT || 4000, () => {
+      console.log("Connected to Database And Server is running, Port:", process.env.PORT || 5000  );
     });
   })
   .catch(e => console.log(e));
